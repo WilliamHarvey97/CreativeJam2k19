641d365b9e53fdf4b9428ac3f03d9b1b85dfd6bf
Claudia Onorato <claudia.onorato@polymtl.ca>
2019-01-12 14:58:04 -0500

    Added TowerBase to add towers

---
+++ BoudeursBeach/Assets/Scripts/TowerBaseEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBaseEditor : MonoBehaviour
+{
+    public GameObject tower;
+    public bool isEmpty = true;
+    public float radius;
+
+    void OnMouseOver() {
+        if(Input.GetMouseButtonDown(0) && isEmpty) {
+            GameObject towerObj = GameObject.Instantiate<GameObject>(tower);
+            towerObj.transform.position = new Vector3(
+                this.transform.position.x,
+                towerObj.transform.localScale.y,
+                this.transform.position.z);
+            TowerLauncher towerLauncher = towerObj.GetComponent<TowerLauncher>();
+            towerLauncher.radius = radius;
+            isEmpty = false;
+        }
+    }
+}
