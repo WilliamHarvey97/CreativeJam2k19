58910a8f2aded520a5c21acf7bd9e4f286334a21
Julien Legault <julien-3.legault@polymtl.ca>
2019-01-12 15:07:35 -0500

    barre de vie ennemies fait

---
+++ BoudeursBeach/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public GameObject followedObject;
+ 
+    void Update()
+    {
+        this.transform.position = Camera.main.WorldToScreenPoint(followedObject.transform.position);
+    }
+}
