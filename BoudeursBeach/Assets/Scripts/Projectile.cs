d0cf7881947c9ad03aabbeab8b46cb5f762e107d
Claudia Onorato <claudia.onorato@polymtl.ca>
2019-01-12 12:21:38 -0500

    Fixed parent bug

--- BoudeursBeach/Assets/Scripts/Projectile.cs
+++ BoudeursBeach/Assets/Scripts/Projectile.cs
@@ -8,12 +8,6 @@ public class Projectile : MonoBehaviour
     public int damagePoints;
     public Collider targetEnemy;
 
-    // Methods
-    void Awake() {
-        // Keep scene heirarchy clean
-        transform.parent = GameObject.FindGameObjectWithTag("Projectiles").transform;
-    }
-
     // Update is called once per frame
     void Update()
     {
