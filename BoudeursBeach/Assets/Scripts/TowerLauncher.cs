16d83a3e1e6d109c2be1fab36ca52be66732014f
Claudia Onorato <claudia.onorato@polymtl.ca>
2019-01-12 00:48:35 -0500

    Tower shoots closests enemy at a determined frequency

---
+++ BoudeursBeach/Assets/Scripts/TowerLauncher.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerLauncher : MonoBehaviour
+{
+    public float radius;
+    public float reloadTime;
+    private float timeBeforeReloaded;
+
+    void Start() {
+        timeBeforeReloaded = 0.0f;
+    }
+
+    void Update()
+    {
+        if(!isReloading()) {
+            Collider[] enemiesInRadius = Physics.OverlapSphere(this.transform.position, radius);
+            if(enemiesInRadius.Length == 0) return;
+
+            Collider closestEnemy = enemiesInRadius[0]; 
+            for(int i = 1; i < enemiesInRadius.Length; ++i) {
+                if(isCurrentEnemyCloser(enemiesInRadius[i], closestEnemy)) {
+                    closestEnemy = enemiesInRadius[i];
+                }
+            }
+
+            AttackEnemy(closestEnemy);
+            timeBeforeReloaded = reloadTime;
+        } else {
+            timeBeforeReloaded -= Time.deltaTime;
+        }
+    }
+
+    bool isCurrentEnemyCloser(Collider currentEnemy, Collider closestEnemy) {
+        return (currentEnemy.transform.position - this.transform.position).magnitude < 
+            (closestEnemy.transform.position - this.transform.position).magnitude;
+    }
+
+    virtual public void AttackEnemy(Collider enemy) {
+        Debug.Log("Attack : " + enemy.name);
+    }
+
+    bool isReloading() {
+        return timeBeforeReloaded > 0.0f;
+    }
+}
