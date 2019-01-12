4f420d9686f369ec68072cf4e4c0af32d0841311
Julien Legault <julien-3.legault@polymtl.ca>
2019-01-12 13:53:56 -0500

    ajout du score et de largent du joueur

--- BoudeursBeach/Assets/Scripts/Player/PlayerController.cs
+++ BoudeursBeach/Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour{
     public float health =10f;
     public float speed =5f;
+    public int score;
+    public int money;
+    public Text moneyText;
+    public Text scoreText;
 
     // Start is called before the first frame update
     void Start(){
+        money = 0;
+        score = 0;
+        SetCountMoney();
+        SetCountScore();
     }
 
     // Update is called once per frame
@@ -35,6 +44,21 @@ public class PlayerController : MonoBehaviour{
             mousePosition.y =this.transform.position.y;
             transform.LookAt(mousePosition);
         }
+    }
+
+    void kill(){
+        money+=10;
+        SetCountMoney();
+        score+=1;
+        SetCountScore();
+    }
+
+    void SetCountMoney(){
+         moneyText.text = "Money: " + money.ToString();
+    }
+
+    void SetCountScore(){
+        scoreText.text = "Score: " + score.ToString();
     }
 
 
