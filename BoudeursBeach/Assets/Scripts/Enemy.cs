581707bce266f3ff263b4269254fcd204adb8646
SPLEEN <SPLEEN@FOORBAR>
2019-01-12 00:44:42 -0500

    PlayerController with mouse and WASD, Enemy pathfinding and creation of EnemyManager class

---
+++ BoudeursBeach/Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour{
+    public float speed =10f;
+    public float aggroFactor =1.5f;
+    public float aggroDistance =10f;
+    Transform player;
+    Transform exit;
+    NavMeshAgent nav;
+
+
+    void Awake (){
+        player =GameObject.FindGameObjectWithTag("Player").transform;
+        exit   =GameObject.FindGameObjectWithTag("Exit").transform;
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+        nav.speed =speed;
+    }
+
+
+    void Update (){
+        this.Move();
+    } 
+
+    void Move(){
+        if(Vector3.Distance(this.transform.position, player.position)>aggroDistance){
+            nav.SetDestination(exit.position);
+            nav.speed =speed;
+        }
+        else{
+            nav.SetDestination(player.position);
+            nav.speed =speed*aggroFactor;
+        }
+    }
+
+    void Attack(){
+
+    }
+    void Die(){
+
+    }
+}
